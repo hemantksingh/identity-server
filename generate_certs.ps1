95d3ecf9ad838554c850115d73cd03ce945a4734
hemantksingh <hemantksingh.hk@gmail.com>
2021-10-05 01:22:49 +0100

    Update readme

--- generate_certs.ps1
+++ generate_certs.ps1
@@ -25,3 +25,5 @@ $certKeyPath = "$certDir\service-identity.pfx"
 $password = ConvertTo-SecureString $certPassword -AsPlainText -Force
 $cert | Export-PfxCertificate -FilePath $certKeyPath -Password $password
 $rootCert = $(Import-PfxCertificate -FilePath $certKeyPath -CertStoreLocation 'Cert:\LocalMachine\Root' -Password $password)
+
+Write-Host "Exported certificate '$($rootCert.FriendlyName)' to trusted root certificates"
